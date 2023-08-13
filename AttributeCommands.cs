8e31ce64bacaab429633cb044625372fb0573998
viknsagit <viknsagitarius@gmail.com>
2023-08-13 17:06:50 +0500

    fix dependencies

--- AttributeCommands.cs
+++ AttributeCommands.cs
@@ -1,25 +1,83 @@
 ﻿using System.Reflection;
 
-using Telegram.Bot.AttributeCommands.Attributes;using Telegram.Bot.AttributeCommands.Exceptions;
-using Telegram.Bot.Types;namespace Telegram.Bot.AttributeCommands{    public class AttributeCommands    {        private readonly Dictionary<string, MethodInfo> _textCommandsMethods;        private readonly Dictionary<string, MethodInfo> _callbackCommandsMethods;        private readonly Dictionary<string, MethodInfo> _replyCommandsMethods;        public AttributeCommands()        {            _textCommandsMethods = new();            _callbackCommandsMethods = new();            _replyCommandsMethods = new();        }
+using Telegram.Bot.AttributeCommands.Attributes;
+using Telegram.Bot.AttributeCommands.Exceptions;
+using Telegram.Bot.Types;
+
+namespace Telegram.Bot.AttributeCommands
+{
+    public class AttributeCommands
+    {
+        private readonly Dictionary<string, MethodInfo> _textCommandsMethods;
+        private readonly Dictionary<string, MethodInfo> _callbackCommandsMethods;
+        private readonly Dictionary<string, MethodInfo> _replyCommandsMethods;
+
+        public AttributeCommands()
+        {
+            _textCommandsMethods = new();
+            _callbackCommandsMethods = new();
+            _replyCommandsMethods = new();
+        }
 
         /// <summary>
         /// Registers text commands from the given class.
         /// </summary>
         /// <param name="commandsClass">The class containing the text commands.</param>
-        public void RegisterTextCommands(Type commandsClass)        {            MethodInfo[] methods = commandsClass.GetMethods();            foreach (MethodInfo method in methods)            {                if (method.GetCustomAttribute<TextCommandAttribute>() != null)                {                    var commandName = method.GetCustomAttribute<TextCommandAttribute>()!.Command;                    if (!_textCommandsMethods.ContainsKey(commandName))                        _textCommandsMethods.Add(commandName, method);                    else                        throw new CommandExistsException(commandName);                }            }        }
+        public void RegisterTextCommands(Type commandsClass)
+        {
+            MethodInfo[] methods = commandsClass.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                if (method.GetCustomAttribute<TextCommandAttribute>() != null)
+                {
+                    var commandName = method.GetCustomAttribute<TextCommandAttribute>()!.Command;
+                    if (!_textCommandsMethods.ContainsKey(commandName))
+                        _textCommandsMethods.Add(commandName, method);
+                    else
+                        throw new CommandExistsException(commandName);
+                }
+            }
+        }
 
         /// <summary>
         /// Registers callback commands from the given class.
         /// </summary>
         /// <param name="commandsClass">The class containing the callback commands.</param>
-        public void RegisterCallbackCommands(Type commandsClass)        {            MethodInfo[] methods = commandsClass.GetMethods();            foreach (MethodInfo method in methods)            {                if (method.GetCustomAttribute<CallbackCommandAttribute>() != null)                {                    var commandName = method.GetCustomAttribute<CallbackCommandAttribute>()!.CallbackCommand;                    if (!_callbackCommandsMethods.ContainsKey(commandName))                        _callbackCommandsMethods.Add(commandName, method);                    else                        throw new CommandExistsException(commandName);                }            }        }
+        public void RegisterCallbackCommands(Type commandsClass)
+        {
+            MethodInfo[] methods = commandsClass.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                if (method.GetCustomAttribute<CallbackCommandAttribute>() != null)
+                {
+                    var commandName = method.GetCustomAttribute<CallbackCommandAttribute>()!.CallbackCommand;
+                    if (!_callbackCommandsMethods.ContainsKey(commandName))
+                        _callbackCommandsMethods.Add(commandName, method);
+                    else
+                        throw new CommandExistsException(commandName);
+                }
+            }
+        }
 
         /// <summary>
         /// Registers the reply commands from the specified commands class.
         /// </summary>
         /// <param name="commandsClass">The commands class.</param>
-        public void RegisterReplyCommands(Type commandsClass)        {            MethodInfo[] methods = commandsClass.GetMethods();            foreach (MethodInfo method in methods)            {                if (method.GetCustomAttribute<ReplyCommandAttribute>() != null)                {                    var commandName = method.GetCustomAttribute<ReplyCommandAttribute>()!.ReplyCommand;                    if (!_replyCommandsMethods.ContainsKey(commandName))                        _replyCommandsMethods[commandName] = method;                    else                        throw new CommandExistsException(commandName);                }            }        }
+        public void RegisterReplyCommands(Type commandsClass)
+        {
+            MethodInfo[] methods = commandsClass.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                if (method.GetCustomAttribute<ReplyCommandAttribute>() != null)
+                {
+                    var commandName = method.GetCustomAttribute<ReplyCommandAttribute>()!.ReplyCommand;
+                    if (!_replyCommandsMethods.ContainsKey(commandName))
+                        _replyCommandsMethods[commandName] = method;
+                    else
+                        throw new CommandExistsException(commandName);
+                }
+            }
+        }
 
         /// <summary>
         /// Gets the command method info by name.
@@ -47,7 +105,16 @@ using Telegram.Bot.Types;namespace Telegram.Bot.AttributeCommands{    public
         /// <returns>
         /// A Task that represents the asynchronous operation.
         /// </returns>
-        public async Task ProcessCommand(string commandName, TelegramBotClient client, Update update)        {            MethodInfo? findedMethod = await GetCommandByNameAsync(commandName);            if (findedMethod is null)                throw new CommandNotFoundException(commandName);            else                findedMethod.Invoke(this, new object[] { client, update });            await Task.CompletedTask;        }
+        public async Task ProcessCommand(string commandName, TelegramBotClient client, Update update)
+        {
+            MethodInfo? findedMethod = await GetCommandByNameAsync(commandName);
+            if (findedMethod is null)
+                throw new CommandNotFoundException(commandName);
+            else
+                findedMethod.Invoke(this, new object[] { client, update });
+
+            await Task.CompletedTask;
+        }
 
         /// <summary>
         /// Processes the command by searching for the corresponding method.
@@ -57,5 +124,15 @@ using Telegram.Bot.Types;namespace Telegram.Bot.AttributeCommands{    public
         /// <returns>
         /// A task that represents the asynchronous operation.
         /// </returns>
-        public async Task ProcessCommand(string commandName, object[] arguments)        {
-            MethodInfo? findedMethod = await GetCommandByNameAsync(commandName);            if (findedMethod is null)                throw new CommandNotFoundException(commandName);            else                findedMethod.Invoke(this, arguments);            await Task.CompletedTask;        }    }}
+        public async Task ProcessCommand(string commandName, object[] arguments)
+        {
+            MethodInfo? findedMethod = await GetCommandByNameAsync(commandName);
+            if (findedMethod is null)
+                throw new CommandNotFoundException(commandName);
+            else
+                findedMethod.Invoke(this, arguments);
+
+            await Task.CompletedTask;
+        }
+    }
+}
